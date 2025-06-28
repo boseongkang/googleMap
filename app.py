3b97a62c7e0196a7d37c9882f1552ae2e1c453a9
boseongkang <okboseong@gmail.com>
2025-06-28 00:26:30 -0700

    up to date

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 GOOGLE_BROWSER_KEY = os.getenv("GOOGLE_BROWSER_KEY")
 GOOGLE_SERVER_KEY = os.getenv("GOOGLE_SERVER_KEY")
 
+
 @app.route('/')
 def home():
     return render_template('home.html', browser_key=GOOGLE_BROWSER_KEY)
@@ -23,7 +24,9 @@ def results():
     geo_params = {'address': location, 'key': GOOGLE_SERVER_KEY}
     geo_resp = requests.get(geo_url, params=geo_params).json()
     if geo_resp.get('status') != 'OK' or not geo_resp.get('results'):
-        return render_template('results.html', error='Invalid location', places=[], center_json=json.dumps({'lat':0,'lng':0}), location_text=location, keyword=keyword, browser_key=GOOGLE_BROWSER_KEY)
+        return render_template('results.html', error='Invalid location', places=[],
+                               center_json=json.dumps({'lat': 0, 'lng': 0}), location_text=location, keyword=keyword,
+                               browser_key=GOOGLE_BROWSER_KEY)
     loc = geo_resp['results'][0]['geometry']['location']
     center_json = json.dumps(loc)
 
@@ -39,12 +42,13 @@ def results():
     places = places_resp.get('results', [])
 
     return render_template('results.html',
-        center_json=center_json,
-        location_text=location,
-        keyword=keyword,
-        places=places,
-        browser_key=GOOGLE_BROWSER_KEY
-    )
+                           center_json=center_json,
+                           location_text=location,
+                           keyword=keyword,
+                           places=places,
+                           browser_key=GOOGLE_BROWSER_KEY
+                           )
+
 
 @app.route('/map_search', methods=['POST'])
 def map_search():
@@ -63,6 +67,7 @@ def map_search():
     places_resp = requests.get(places_url, params=place_params).json()
     places = places_resp.get('results', [])
     return jsonify({"places": places})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
